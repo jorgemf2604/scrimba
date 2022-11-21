e1ee36b6c1c1c27c49ad2bb9b9b92561cc4e6e27
jorgemf2604 <jorgemf2604@gmail.com>
2022-11-21 12:34:27 +0000

    completed app

--- twimba/index.js
+++ twimba/index.js
@@ -1,24 +1,101 @@
 import {tweetsData} from "./data.js";
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 // load initial data 
-let tweets = tweetsData; 
+let tweets = JSON.parse(localStorage.getItem("tweets")) || tweetsData; 
 
 // DOM bindings 
 const tweetsContainer = document.getElementById("feed");
 
 // render intial data 
-renderTweets()
+renderTweets();
 
 // event listeners
 document.body.addEventListener("click", (e) => {
-    if(e.target.dataset.likes) {
-        console.log(e.target.dataset.likes)
+    if (e.target.dataset.likes) {
+        handleLikeIconClick(e.target.dataset.likes);
+    } else if (e.target.dataset.comments) {
+        handleCommentsIconClick(e.target.dataset.comments);
+    } else if (e.target.dataset.retweets) {
+        handleRetweetsIconClick(e.target.dataset.retweets);
+    } else if (e.target.id === "tweet-btn") {
+        handleTweetBtnClick();
+    } else if (e.target.dataset.addcomment) {
+        handleCommentBtnClick(e.target.dataset.addcomment);
     }
 })
+
+// event handlers 
+function handleCommentBtnClick(tweetID) {
+    const targetTweet = tweets.filter(tweet => tweet.uuid === tweetID)[0];
+    const commentInput = document.getElementById(`comment-input-${tweetID}`);
+    if (commentInput.value !== "") {
+        targetTweet.replies.unshift({
+            handle: `@Battousai 🥷🥷`,
+            profilePic: `images/battousai.jpeg`,
+            tweetText: `${commentInput.value}`,
+        });
+        saveTweets();
+        renderTweets();
+        commentInput.value = "";
+    }
+}
+
+function handleLikeIconClick(tweetID) {
+    const targetTweet = tweets.filter(tweet => tweet.uuid === tweetID)[0];
+    if (targetTweet.isLiked) {
+        targetTweet.likes--;
+    } else {
+        targetTweet.likes++;
+    }
+    targetTweet.isLiked = !targetTweet.isLiked;
+    renderTweets();
+    saveTweets();
+}
+
+function handleRetweetsIconClick(tweetID) {
+    const targetTweet = tweets.filter(tweet => tweet.uuid === tweetID)[0];
+    if (targetTweet.isRetweeted) {
+        targetTweet.retweets--;
+    } else {
+        targetTweet.retweets++;
+    }
+    targetTweet.isRetweeted = !targetTweet.isRetweeted;
+    renderTweets();
+    saveTweets();
+}
+
+function handleCommentsIconClick(tweetID) {
+    document.getElementById(`comments-${tweetID}`).classList.toggle("hidden");
+}
+
+function handleTweetBtnClick() {
+    const inputEl = document.getElementById("main-input");
+    if (inputEl.value !== "") {
+        tweets.unshift({
+            handle: `@Battousai 🥷🥷`,
+            profilePic: `images/battousai.jpeg`,
+            likes: 0,
+            retweets: 0,
+            tweetText: `${inputEl.value}`,
+            replies: [],
+            isLiked: false,
+            isRetweeted: false,
+            uuid: uuidv4(),
+        });
+        inputEl.value = "";
+        saveTweets();
+        renderTweets();
+    }
+}
 
 // helper functions 
 function renderTweets() {
     tweetsContainer.innerHTML = generateTweetsHTML();
+}
+
+function saveTweets() {
+    localStorage.setItem("tweets", JSON.stringify(tweets));
 }
 
 function generateTweetsHTML() {
@@ -40,6 +117,10 @@ function generateTweetsHTML() {
             `;
         })
 
+        // cosmetic class for icons 
+        const likeIconClass = tweet.isLiked ? "likedIcon" : "";
+        const retweetIconClass = tweet.isRetweeted ? "retweetIcon" : "";
+
         // generate complete HTML template
         htmlTemplate += `
             <div class="tweet">
@@ -50,16 +131,16 @@ function generateTweetsHTML() {
                         <span class="tweet-handle">${tweet.handle}</span>
                         <p class="tweet-text">${tweet.tweetText}</p>
                         <div class="tweet-metrics">
-                            <span class="tweet-metric"><i class="fa-solid fa-comments"></i>${tweet.replies.length}</span>
-                            <span class="tweet-metric"><i class="fa-solid fa-heart" data-likes="${tweet.uuid}"></i>${tweet.likes}</span>
-                            <span class="tweet-metric"><i class="fa-solid fa-retweet"></i>${tweet.retweets}</span>
+                            <span class="tweet-metric"><i class="fa-solid fa-comments" data-comments="${tweet.uuid}"></i>${tweet.replies.length}</span>
+                            <span class="tweet-metric"><i class="fa-solid fa-heart ${likeIconClass}" data-likes="${tweet.uuid}"></i>${tweet.likes}</span>
+                            <span class="tweet-metric"><i class="fa-solid fa-retweet ${retweetIconClass}" data-retweets="${tweet.uuid}"></i>${tweet.retweets}</span>
                         </div>
                     </div>
                 </div>
-                <div class="comments hidden">
+                <div class="comments hidden" id="comments-${tweet.uuid}">
                     <div class="add-comment-container">
-                        <input class="add-comment-input" placeholder="Add your comment here..." />
-                        <button class="add-comment-btn">Tweet</button>
+                        <input class="add-comment-input" id="comment-input-${tweet.uuid}" placeholder="Add your comment here..." />
+                        <button class="add-comment-btn" data-addcomment="${tweet.uuid}">Tweet</button>
                     </div>
                     ${templateComments}
                 </div>
